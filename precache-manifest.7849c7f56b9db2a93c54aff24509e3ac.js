6d9a3fbf71e28400de5ebef0cb42ea44f42397f7
Melanie Panem <melanie.panem@creditone.com>
2020-08-29 10:21:40 -0700

    weird gh-pages stuff

---
+++ precache-manifest.7849c7f56b9db2a93c54aff24509e3ac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd3d837ef7633b43482ee682dbd59b35",
+    "url": "/mysavei/index.html"
+  },
+  {
+    "revision": "f93586d171f8b8761dd6",
+    "url": "/mysavei/static/css/main.ee5a4476.chunk.css"
+  },
+  {
+    "revision": "8f12d7f66bf3fe7af1a9",
+    "url": "/mysavei/static/js/2.a1cf8c3b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/mysavei/static/js/2.a1cf8c3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f93586d171f8b8761dd6",
+    "url": "/mysavei/static/js/main.f7dd2987.chunk.js"
+  },
+  {
+    "revision": "f4e51f36081e6b2bb290",
+    "url": "/mysavei/static/js/runtime-main.6ffa9c1e.js"
+  },
+  {
+    "revision": "292feff0ab3202abf442dc1fef5fece9",
+    "url": "/mysavei/static/media/acorns.292feff0.png"
+  },
+  {
+    "revision": "61bd9af17c09d0a6b75c76b2277a4755",
+    "url": "/mysavei/static/media/robinhood.61bd9af1.png"
+  }
+]);
