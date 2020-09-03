93b279d581056fce24157e2eef54c6c615a086b0
Melanie Panem <melanie.panem@creditone.com>
2020-09-03 15:11:09 -0700

    Updates

---
+++ precache-manifest.6d44ac6dfb361f6eea5952941cb2a857.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e7a7e016ca62d1a8df417e4f0576c2a",
+    "url": "/mysavei/index.html"
+  },
+  {
+    "revision": "0574dc2210b7db743584",
+    "url": "/mysavei/static/css/main.fe12250c.chunk.css"
+  },
+  {
+    "revision": "73ffc9fe6aa6fb7698dd",
+    "url": "/mysavei/static/js/2.f5f3b349.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/mysavei/static/js/2.f5f3b349.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0574dc2210b7db743584",
+    "url": "/mysavei/static/js/main.3b621e5c.chunk.js"
+  },
+  {
+    "revision": "f4e51f36081e6b2bb290",
+    "url": "/mysavei/static/js/runtime-main.6ffa9c1e.js"
+  },
+  {
+    "revision": "292feff0ab3202abf442dc1fef5fece9",
+    "url": "/mysavei/static/media/acorns.292feff0.png"
+  },
+  {
+    "revision": "61bd9af17c09d0a6b75c76b2277a4755",
+    "url": "/mysavei/static/media/robinhood.61bd9af1.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2dd2dd731d3f439af435470b62506d2f.js"
+  "/mysavei/precache-manifest.6d44ac6dfb361f6eea5952941cb2a857.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mysavei/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
