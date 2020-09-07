559d0cf58d72444d7a866f2d4e51aaa7c5763993
Melanie Panem <melanie.panem@creditone.com>
2020-09-07 11:46:47 -0700

    fixing the meta tags and device view tags in the correct public file now

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './css/mysavei.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    
     <App/>
 
   </React.StrictMode>,
