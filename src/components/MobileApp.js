765cfed22a6c083444411f31685d51f05ca11e36
Melanie Panem <melanie.panem@creditone.com>
2020-09-11 18:46:11 -0700

    fixed app store modals on the All page

--- src/components/MobileApp.js
+++ src/components/MobileApp.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import AcornsImg from '../imgs/acorns.png';
 import RobinhoodImg from '../imgs/robinhood.png';
-import AppStoreIcons from './AppStoreIcons.js';
+import ModalAcorns from './ModalAcorns.js';
+import ModalRobinhood from './ModalRobinhood.js';
 
 
 
@@ -12,19 +13,32 @@ const MobileApp = ({type, containAll, containDescr, expand}) => {
     return (
         <article id="mobile-app" className="row component-content mobile-app">
             {/* modal for app stores */}
-            <AppStoreIcons id="app-store-modal" type={type ? true : false} />
+            <ModalAcorns />
+            <ModalRobinhood />
 
-            <section className="img-container col-12 col-md-3" onClick={() => document.getElementById('app-store-icons').style.display = "block"}>
+            <section className="img-container col-12 col-md-3" onClick={type ?
+                () => document.getElementById('acorns-store-icon').style.display = "block"
+                :
+                () => document.getElementById('robinhood-store-icon').style.display = "block"
+            }>
                 <div className="img-holder">
                     <img 
                         className="app-icons" 
                         src={ type ? AcornsImg : RobinhoodImg } 
-                        onClick={() => document.getElementById('app-store-icons').style.display = "block"} />
+                        onClick={ type ?
+                            () => document.getElementById('acorns-store-icon').style.display = "block"
+                        :
+                            () => document.getElementById('robinhood-store-icon').style.display = "block"
+                        } />
                 </div>
             </section>
 
             <section className={`about-mobile-app col-12 col-md-9 ${ containAll ? 'dropdown-start' : '' }`}>
-                <div className="mobile-app-title" onClick={() => document.getElementById('app-store-icons').style.display = "block"}>
+                <div className="mobile-app-title" onClick={type ?
+                    () => document.getElementById('acorns-store-icon').style.display = "block"
+                    :
+                    () => document.getElementById('robinhood-store-icon').style.display = "block"
+                } >
                     <h4 className="mobile-app-title"><span className="type">{ type ? 'Acorns' : 'Robinhood' }</span> mobile app</h4>
                 </div>
                 <div className={`mobile-app-descr ${ containDescr ? 'dropdown-start' : '' }`}>
---
+++ src/components/ModalAcorns.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const ModalAcorns = () => {
+
+    return (
+        <section id="acorns-store-icon" className="text-center" style={{display:'none'}}>
+            <div className="close" onClick={() => document.getElementById('acorns-store-icon').style.display = "none"}>
+                <i className="fas fa-times"></i>
+            </div>
+            <h5>Which app store?</h5>
+            <div className="apps">
+                <a className="app-store-link google" href='https://play.google.com/store/apps/details?id=com.acorns.android' target='_blank' style={{ padding: '5px 10px 5px 20px' }}>
+                    <i className="fab fa-google-play"></i><br />
+                    <span className="google-link">Google Play</span>
+                </a>
+                <a className="app-store-link apple" href='https://apps.apple.com/us/app/acorns-invest-spare-change/id883324671' target='_blank' style={{ padding: '5px 20px 5px 10px' }}>
+                    <i className="fab fa-apple"></i><br />
+                    <span className="apple-link">Apple Store</span>
+                </a>
+            </div>
+        </section>
+    )
+}
+
+export default ModalAcorns;
