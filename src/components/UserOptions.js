fe66d34beba7cedb10e88ba693895183f3d619a9
Melanie Panem <melanielynnpanem@gmail.com>
2021-01-17 01:27:10 -0800

    some minor work on the second step

--- src/components/UserOptions.js
+++ src/components/UserOptions.js
@@ -5,8 +5,8 @@ const UserOptions = () => (
     <nav>
         <ul className="options-list">
             <li id="passive-option">
-                <Link to={process.env.PUBLIC_URL + '/passive'}>
-                    <span className="fw-semibold">I want to invest my money passively,<br/>long-term.</span>
+                <Link to={process.env.PUBLIC_URL + '/passive'} className="option-link">
+                    <span className="fw-semibold more-head">Passively,<br/>Long-Term</span>
                     <br /><br className="hide-mobile"/>
                     <span className="more-descr">i.e. not check your account or participate in the investment process once it’s started</span>
                     <div className="bottom-2">
@@ -15,16 +15,16 @@ const UserOptions = () => (
                 </Link>
             </li>
             <li id="both-option">
-                <Link to={process.env.PUBLIC_URL + '/all'}>
-                    <span className="fw-semibold">What about a little of both? <i className="mobile-arrow fas fa-chevron-right"></i> </span>
+                <Link to={process.env.PUBLIC_URL + '/all'} className="option-link">
+                    <span className="fw-semibold more-head">Both? <i className="mobile-arrow fas fa-chevron-right"></i> </span>
                     <div className="bottom-2">
                         <i className="fas fa-chevron-right"></i>
                     </div>
                 </Link>
             </li>
             <li id="active-option">
-                <Link to={process.env.PUBLIC_URL + '/active'}>
-                    <span className="fw-semibold">I want to invest actively in the stock market.</span>
+                <Link to={process.env.PUBLIC_URL + '/active'} className="option-link">
+                    <span className="fw-semibold more-head">Actively<br/> Trade</span>
                     <br/><br className="hide-mobile"/>
                     <span className="more-descr">i.e. you want to learn a little bit more about stocks, do some short-term trading, and setup
                     re-occurring deposits</span>
